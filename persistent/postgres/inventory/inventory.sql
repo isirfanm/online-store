456041fcffee8dd83616cdef302e5066b07458e7
irfan <irfan@volantis.io>
2021-08-27 22:31:15 +0700

    fix inventory.sql

--- persistent/postgres/inventory/inventory.sql
+++ persistent/postgres/inventory/inventory.sql
@@ -1,12 +1,12 @@
 -- product table
-create or replace table public.product (
+create table public.product (
     sku text not null,
     stock integer not null,
     constraint product_pk primary key (sku)
 )
 
 -- order table
-create or replace table public."order" (
+create table public."order" (
     id uuid not null,
     sku text not null references product(sku),
     quantity integer not null,
